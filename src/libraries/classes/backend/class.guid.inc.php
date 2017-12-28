53c21e0c30f54fb064d41677993d26b59526b5ab
bimal <gist@bimal.org.np>
2017-12-28 08:15:55 -0700

    cleaning

--- src/libraries/classes/backend/class.guid.inc.php
+++ src/libraries/classes/backend/class.guid.inc.php
@@ -14,17 +14,17 @@ class guid
 	 * 
 	 * @return string
 	 */
-    public static function NewGuid(): string
-    {
-        mt_srand((double)microtime()*10000);
+	public static function NewGuid(): string
+	{
+		mt_srand((double)microtime()*10000);
 
 		$rand = mt_rand(10000, 99999);
 		$uniqid = uniqid(mt_rand(), true);
 		$timestamp = date("HisYmd");
-        
+
 		$characters = strtoupper(md5($rand.$uniqid.$timestamp));
 		$guid = preg_replace("/^([0-9A-F]{8})([0-9A-F]{4})([0-9A-F]{4})([0-9A-F]{4})([0-9A-F]{12})$/is", "$1-$2-$3-$4-$5", $characters);
 
-        return $guid;
-    }
+		return $guid;
+	}
 }
