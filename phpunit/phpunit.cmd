ed9f7860633f7165932bae46cd7279f74997608a
server <server@example.com>
2018-02-11 08:03:08 -0700

    composer

--- phpunit/phpunit.cmd
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-REM IF NOT EXIST phpunit.phar curl -L -o phpunit.phar https://phar.phpunit.de/phpunit.phar
-REM @php ".\phpunit.phar" --bootsrap phpunit/bootstrap.php --c phpunit/phpunit.xml
-REM @php ".\phpunit.phar"
-
-phpunit
-CALL MORE logs\testdox.txt
