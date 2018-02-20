3964987905eb35236d6b5969a867ce0d1954e595
server <server@example.com>
2018-02-20 12:08:00 -0700

    Test using vendor

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace tests;
 
-require_once("src/libraries/classes/backend/class.guid.inc.php");
+require_once("vendor/autoload.php");
 use backend\guid;
 
 $g = new guid();
