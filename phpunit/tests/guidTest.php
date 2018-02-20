3e7b5be600f98228c1fb86b5ce28497e2b1dcc8e
server <server@example.com>
2018-02-20 12:59:15 -0700

    unit tests

--- phpunit/tests/guidTest.php
+++ phpunit/tests/guidTest.php
@@ -6,51 +6,38 @@ use PHPUnit\Framework\TestCase;
 
 class guidTest extends TestCase
 {
-    public function setup()
-    {
-    }
+	private $guid;
 
-    public function testGuidLength()
-    {
+	public function setup()
+	{
 		$g = new guid();
-		
-		$guid = $g->NewGuid();
-        
-		$this->assertEquals(36, strlen($guid));
-    }
-	
+		$this->guid = $g->NewGuid();
+	}
+
+	public function testGuidLength()
+	{
+
+		$this->assertEquals(36, strlen($this->guid));
+	}
+
 	public function testGuidUpperCased()
-    {
-		$g = new guid();
-		
-		$guid = $g->NewGuid();
-        
-		$this->assertEquals($guid, strtoupper($guid));
-    }
-	
+	{
+		$this->assertEquals($this->guid, strtoupper($this->guid));
+	}
+
 	public function testGuidComponentsCount()
-    {
-		$g = new guid();
-		
-		$guid = $g->NewGuid();
-		
+	{
 		$matches = array();
-		$matches = explode("-", $guid);
-		#print_r($matches);
-        
+		$matches = explode("-", $this->guid);
+
 		$this->assertEquals(5, count($matches));
-    }
-	
+	}
+
 	public function testGuidRegularExpressions()
-    {
-		$g = new guid();
-		
-		$guid = $g->NewGuid();
-		
+	{
 		$matches = array();
-		preg_match("/^([0-9A-Z]{8})\\-([0-9A-Z]{4})\\-([0-9A-Z]{4})\\-([0-9A-Z]{4})\\-([0-9A-Z]{12})$/s", $guid, $matches, PREG_OFFSET_CAPTURE);
-		#print_r($matches);
-        
+		preg_match("/^([0-9A-Z]{8})\\-([0-9A-Z]{4})\\-([0-9A-Z]{4})\\-([0-9A-Z]{4})\\-([0-9A-Z]{12})$/s", $this->guid, $matches, PREG_OFFSET_CAPTURE);
+
 		$this->assertEquals(1+5, count($matches));
-    }
+	}
 }
