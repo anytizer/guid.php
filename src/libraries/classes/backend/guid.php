818996815860cff839f549148346388207e2575f
server <server@example.com>
2018-02-21 10:49:26 -0700

    Remove statics

--- src/libraries/classes/backend/guid.php
+++ src/libraries/classes/backend/guid.php
@@ -1,2 +1,30 @@
 <?php
-require_once(dirname(__FILE__)."/class.guid.inc.php");
+namespace backend;
+
+/**
+ * GUID Generator
+ * @package backend
+ *
+ * @see http://guid.us/GUID/PHP
+ */
+class guid
+{
+	/**
+	 * Generate a new GUID
+	 * 
+	 * @return string
+	 */
+	public static function NewGuid(): string
+	{
+		mt_srand((double)microtime(false)*10000);
+
+		$rand = mt_rand(100000, 999999);
+		$timestamp = date("HisYmd");
+		$uniqid = uniqid(mt_rand(), true);
+
+		$characters = strtoupper(md5($rand.$timestamp.$uniqid));
+		$guid = preg_replace("/^([0-9A-F]{8})([0-9A-F]{4})([0-9A-F]{4})([0-9A-F]{4})([0-9A-F]{12})$/is", "$1-$2-$3-$4-$5", $characters);
+
+		return $guid;
+	}
+}
--- src/static.php
+++
@@ -1,7 +0,0 @@
-<?php
-use anytizer\includer;
-
-/**
- * You can register multiple paths
- */
-spl_autoload_register(array(new includer("../vendor/anytizer/guid.php/src/libraries/classes"), "namespaced_inc_dot"));
